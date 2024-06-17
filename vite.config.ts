420408855691f52a2bf44fc42f80e97e9e2eb2f8
Kien Duong Trung Nguyen <kien.nguyen@asnet.com.vn>
2024-06-17 16:46:13 +0700

    feat: install libs necessary

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import viteCompression from "vite-plugin-compression";
-import react from "@vitejs/plugin-react";
-import EnvironmentPlugin from "vite-plugin-environment";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from 'vite';
+import viteCompression from 'vite-plugin-compression';
+import react from '@vitejs/plugin-react';
+import EnvironmentPlugin from 'vite-plugin-environment';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
-    EnvironmentPlugin("all"),
+    EnvironmentPlugin('all'),
     viteCompression(),
   ],
 
