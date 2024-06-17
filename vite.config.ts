13382535ca2cb6e24017fd3617fe821f09a92df9
Kien Duong Trung Nguyen <kien.nguyen@asnet.com.vn>
2024-06-17 16:33:52 +0700

    feat: config enviroment for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import viteCompression from "vite-plugin-compression";
+import react from "@vitejs/plugin-react";
+import EnvironmentPlugin from "vite-plugin-environment";
+import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    EnvironmentPlugin("all"),
+    viteCompression(),
+  ],
+
+  server: {
+    port: 3000,
+  },
+});
