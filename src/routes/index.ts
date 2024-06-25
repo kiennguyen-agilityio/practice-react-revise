552c704710f7c1a8a38b02069cded315fcd72bf1
Kien Duong Trung Nguyen <kien.nguyen@asnet.com.vn>
2024-06-25 11:53:08 +0700

    feat: build detail page

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,11 @@
-import { ROUTES } from '@app/constants/routers';
-import HomePage from '@app/pages/Homepage';
 import { createBrowserRouter } from 'react-router-dom';
+
+// constants
+import { ROUTES } from '@app/constants/routers';
+
+// pages
+import DetailPage from '@app/pages/Detailpage';
+import HomePage from '@app/pages/Homepage';
 
 export const ROUTER = createBrowserRouter([
   {
@@ -9,6 +14,11 @@ export const ROUTER = createBrowserRouter([
       {
         index: true,
         Component: HomePage,
+      },
+
+      {
+        index: true,
+        Component: DetailPage,
       },
     ],
   },
